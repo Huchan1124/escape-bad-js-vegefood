4379bd330a0efc5ab0c1e311f1ea4ad6f2a9811e
Huchan1124 <garlic1124@gmail.com>
2022-03-08 23:32:52 +0800

    根據ESLint與TODO提示修改code

--- js/all.js
+++ js/all.js
@@ -1,57 +1,61 @@
 // TODO: 修正 ESLint 錯誤、補上分號、前輩說要改單引號 QQ
-var url="https://hexschool.github.io/js-filter-data/data.json"
-var data;
+const url = 'https://hexschool.github.io/js-filter-data/data.json';
+const table = document.querySelector('.table-content');
+const filter = document.querySelector('.filter');
+let data;
+let category = '';
+let showData = [];
 
-axios.get(url)
- .then(function(res){
-  data=res.data.filter(a=>a.作物名稱)
-  // TODO: 之後拆成 renderData 函式
-  var str=""
-  data.forEach((b,index)=>{
-    // TODO: 改成 ES6 的 Template Literals (字面字串符)
-    var content="<tr><td>" + b.作物名稱 
-    +"</td><td>" + b.市場名稱
-    +"</td><td>" + b.上價
-    +"</td><td>" + b.中價
-    +"</td><td>" + b.下價
-    +"</td><td>" + b.平均價
-    +"</td><td>" + b.交易量
-    +"</td></tr>"
-    str+=content
-  })
-  table.innerHTML=str
-})
+function renderData(dataName) {
+  let str = '';
 
-var table=document.querySelector(".table-content")
-var showData=[]
+  switch (dataName) {
+    case data:
+      data.forEach((b) => {
+        // TODO: 改成 ES6 的 Template Literals (字面字串符)
+        const content = `<tr><td>${b.作物名稱}
+       </td><td>${b.市場名稱}
+       </td><td>${b.上價}
+       </td><td>${b.中價}
+       </td><td>${b.下價}
+       </td><td>${b.平均價}
+       </td><td>${b.交易量}
+       </td></tr>`;
+        str += content;
+      });
+      break;
+    case showData:
+      showData.forEach((b) => {
+        const content = `<tr><td>${b.作物名稱}
+         </td><td>${b.市場名稱}
+         </td><td>${b.上價}
+         </td><td>${b.中價}
+         </td><td>${b.下價}
+         </td><td>${b.平均價}
+         </td><td>${b.交易量}
+         </td></tr>`;
+        str += content;
+      });
+      break;
+    default:
+  }
 
-var category=""
-var filter=document.querySelector(".filter")
+  table.innerHTML = str;
+}
 
-filter.addEventListener("click",filterCategory)
-
-function filterCategory(e){
-  if(e.target.nodeName=="BUTTON"){
-    category=e.target.dataset.category
-    showData=data.filter((i)=>{
-      return i.種類代碼==category
-    })
+function filterCategory(e) {
+  if (e.target.nodeName === 'BUTTON') {
+    category = e.target.dataset.category;
+    showData = data.filter((i) => i.種類代碼 === category);
     // TODO: 之後拆成 renderData 函式
-    var str=""
-    showData.forEach((i,index)=>{
-      var content="<tr><td>" + i.作物名稱 
-      +"</td><td>" + i.市場名稱
-      +"</td><td>" + i.上價
-      +"</td><td>" + i.中價
-      +"</td><td>" + i.下價
-      +"</td><td>" + i.平均價
-      +"</td><td>" + i.交易量
-      +"</td></tr>"
-      str+=content
-    })
-    table.innerHTML=str
-  }else{
-    return;
+    renderData(showData);
   }
 }
 
+axios.get(url)
+  .then((res) => {
+    data = res.data.filter((a) => a.作物名稱);
+    renderData(data);
+  });
+
+filter.addEventListener('click', filterCategory);
